5451981cc72b51cbd5edb85e120c338bcff8911e
barob4 <rob17051@byui.edu>
2021-01-08 10:52:09 -0700

    lesson 1 7th commit

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
 document.getElementById("currYear").textContent = new Date().getFullYear();
 
-let daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday", 
-    "Saturday"
-];
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "Octover",
-    "November",
-    "December"
-];
+document.getElementById("modified").textContent = document.lastModified
 
 
